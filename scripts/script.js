1db2a8f2f91fb0ef8391506bc7aba0f63248ff94
David <146224919+davidlreg@users.noreply.github.com>
2024-11-19 16:52:21 +0100

    structure changes and function for displaying question and answers

---
+++ scripts/script.js
@@ -0,0 +1,31 @@
+let currentQuestion = 0;
+
+function init() {
+  loadPages();
+  showCurrentQuestion();
+  showCurrentAnswerOptions();
+}
+
+function loadPages() {
+  let totalPagesRef = document.getElementById("totalPagesAmount");
+
+  totalPagesRef.innerHTML = "";
+  totalPagesRef.innerHTML = questions.length;
+}
+
+function showCurrentQuestion() {
+  let currentQuestionTitleRef = document.getElementById("titleQuestion");
+  let question = questions[currentQuestion].question;
+
+  currentQuestionTitleRef.innerHTML = "";
+  currentQuestionTitleRef.innerHTML = question;
+}
+
+function showCurrentAnswerOptions() {
+  let question = questions[currentQuestion];
+
+  document.getElementById("answer_1").innerHTML = question["answer_1"];
+  document.getElementById("answer_2").innerHTML = question["answer_2"];
+  document.getElementById("answer_3").innerHTML = question["answer_3"];
+  document.getElementById("answer_4").innerHTML = question["answer_4"];
+}
